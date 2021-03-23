65cda2860947f7ebd9dc74a2a12b06428063229f
crskycode <crskycode@hotmail.com>
2021-03-24 03:30:56 +0800

    Add project files.

---
+++ TextEncodingTool/Main.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace TextEncodingTool
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._textBox_text = new System.Windows.Forms.TextBox();
+            this._comboBox_codePage = new System.Windows.Forms.ComboBox();
+            this._button_encode = new System.Windows.Forms.Button();
+            this._button_decode = new System.Windows.Forms.Button();
+            this._textBox_code = new System.Windows.Forms.TextBox();
+            this._comboBox_codeStyle = new System.Windows.Forms.ComboBox();
+            this._checkBox_autoCopy = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // _textBox_text
+            // 
+            this._textBox_text.Location = new System.Drawing.Point(12, 12);
+            this._textBox_text.Multiline = true;
+            this._textBox_text.Name = "_textBox_text";
+            this._textBox_text.Size = new System.Drawing.Size(776, 176);
+            this._textBox_text.TabIndex = 0;
+            // 
+            // _comboBox_codePage
+            // 
+            this._comboBox_codePage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this._comboBox_codePage.FormattingEnabled = true;
+            this._comboBox_codePage.Location = new System.Drawing.Point(12, 194);
+            this._comboBox_codePage.Name = "_comboBox_codePage";
+            this._comboBox_codePage.Size = new System.Drawing.Size(240, 25);
+            this._comboBox_codePage.TabIndex = 1;
+            // 
+            // _button_encode
+            // 
+            this._button_encode.Location = new System.Drawing.Point(564, 194);
+            this._button_encode.Name = "_button_encode";
+            this._button_encode.Size = new System.Drawing.Size(109, 25);
+            this._button_encode.TabIndex = 2;
+            this._button_encode.Text = "👇编码";
+            this._button_encode.UseVisualStyleBackColor = true;
+            this._button_encode.Click += new System.EventHandler(this.Button_encode_Click);
+            // 
+            // _button_decode
+            // 
+            this._button_decode.Location = new System.Drawing.Point(679, 194);
+            this._button_decode.Name = "_button_decode";
+            this._button_decode.Size = new System.Drawing.Size(109, 25);
+            this._button_decode.TabIndex = 3;
+            this._button_decode.Text = "👆解码";
+            this._button_decode.UseVisualStyleBackColor = true;
+            this._button_decode.Click += new System.EventHandler(this.Button_decode_Click);
+            // 
+            // _textBox_code
+            // 
+            this._textBox_code.Location = new System.Drawing.Point(12, 225);
+            this._textBox_code.Multiline = true;
+            this._textBox_code.Name = "_textBox_code";
+            this._textBox_code.Size = new System.Drawing.Size(776, 213);
+            this._textBox_code.TabIndex = 4;
+            // 
+            // _comboBox_codeStyle
+            // 
+            this._comboBox_codeStyle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this._comboBox_codeStyle.FormattingEnabled = true;
+            this._comboBox_codeStyle.Location = new System.Drawing.Point(258, 194);
+            this._comboBox_codeStyle.Name = "_comboBox_codeStyle";
+            this._comboBox_codeStyle.Size = new System.Drawing.Size(130, 25);
+            this._comboBox_codeStyle.TabIndex = 5;
+            // 
+            // _checkBox_autoCopy
+            // 
+            this._checkBox_autoCopy.AutoSize = true;
+            this._checkBox_autoCopy.Location = new System.Drawing.Point(394, 196);
+            this._checkBox_autoCopy.Name = "_checkBox_autoCopy";
+            this._checkBox_autoCopy.Size = new System.Drawing.Size(147, 21);
+            this._checkBox_autoCopy.TabIndex = 6;
+            this._checkBox_autoCopy.Text = "编码结果复制到剪切板";
+            this._checkBox_autoCopy.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._checkBox_autoCopy);
+            this.Controls.Add(this._comboBox_codeStyle);
+            this.Controls.Add(this._textBox_code);
+            this.Controls.Add(this._button_decode);
+            this.Controls.Add(this._button_encode);
+            this.Controls.Add(this._comboBox_codePage);
+            this.Controls.Add(this._textBox_text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "文本编码工具   作者：展鸿丶";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox _textBox_text;
+        private System.Windows.Forms.ComboBox _comboBox_codePage;
+        private System.Windows.Forms.Button _button_encode;
+        private System.Windows.Forms.Button _button_decode;
+        private System.Windows.Forms.TextBox _textBox_code;
+        private System.Windows.Forms.ComboBox _comboBox_codeStyle;
+        private System.Windows.Forms.CheckBox _checkBox_autoCopy;
+    }
+}
+
---
+++ TextEncodingTool/Main.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEncodingTool
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            _comboBox_codePage.Items.Add("utf-8");
+            _comboBox_codePage.Items.Add("utf-16le");
+            _comboBox_codePage.Items.Add("utf-16be");
+            _comboBox_codePage.Items.Add("gbk");
+            _comboBox_codePage.Items.Add("shift-jis");
+
+            _comboBox_codeStyle.Items.Add("Hex Space");
+            _comboBox_codeStyle.Items.Add("Hex");
+            _comboBox_codeStyle.Items.Add("C String");
+            _comboBox_codeStyle.Items.Add("C Array");
+
+            _comboBox_codeStyle.SelectedIndex = 0;
+        }
+
+        private void Button_encode_Click(object sender, EventArgs e)
+        {
+            if (_comboBox_codePage.SelectedIndex == -1)
+                return;
+
+            if (_textBox_text.TextLength == 0)
+                return;
+
+            byte[] bytes;
+            try
+            {
+                Encoding encoding = Encoding.GetEncoding(_comboBox_codePage.SelectedItem as string);
+                bytes = encoding.GetBytes(_textBox_text.Text);
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(this, x.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            switch (_comboBox_codeStyle.SelectedIndex)
+            {
+                case 0: // Hex Space
+                {
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        if (i != 0)
+                            sb.Append(' ');
+                        sb.Append(bytes[i].ToString("X2"));
+                    }
+                    break;
+                }
+                case 1: // Hex
+                {
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        sb.Append(bytes[i].ToString("X2"));
+                    }
+                    break;
+                }
+                case 2: // C String
+                {
+                    sb.Append('"');
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        sb.Append($"\\x{bytes[i]:X2}");
+                    }
+                    sb.Append('"');
+                    break;
+                }
+                case 3: // C Array
+                {
+                    sb.Append("{ ");
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        if (i != 0)
+                            sb.Append(", ");
+                        sb.Append($"0x{bytes[i]:X2}");
+                    }
+                    sb.Append(" }");
+                    break;
+                }
+            }
+
+            _textBox_code.Text = sb.ToString();
+
+            if (_checkBox_autoCopy.Checked)
+            {
+                Clipboard.SetText(_textBox_code.Text);
+            }
+        }
+
+        private void Button_decode_Click(object sender, EventArgs e)
+        {
+            if (_comboBox_codePage.SelectedIndex == -1)
+                return;
+
+            if (_textBox_code.TextLength == 0)
+                return;
+
+            StringBuilder sb = new StringBuilder(_textBox_code.Text.Length);
+
+            for (int i = 0; i < _textBox_code.Text.Length; i++)
+            {
+                char c = _textBox_code.Text[i];
+                if (c != ' ' && !(c >= '0' && c <= '9') && !(c >= 'A' && c <= 'Z'))
+                {
+                    MessageBox.Show(this, "Invalid code.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                if (c == ' ')
+                    continue;
+
+                sb.Append(c);
+            }
+
+            try
+            {
+                byte[] bytes = Convert.FromHexString(sb.ToString());
+                Encoding encoding = Encoding.GetEncoding(_comboBox_codePage.SelectedItem as string);
+                _textBox_text.Text = encoding.GetString(bytes);
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(this, x.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
